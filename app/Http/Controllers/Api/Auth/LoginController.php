3342f372f277d808fd2adec812d9e7a4576dd03a
Vr <veridetta@gmail.com>
2023-11-04 13:03:52 +0700

    api login

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+
+            return (new UserResource($user))->additional([
+                'token' => $user->createToken('myAppToken')->plainTextToken,
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Your credential does not match.',
+        ], 401);
+    }
+}
